b85f7f97cd9ac6e37bde19c0ef428de88c77dbf9
felipe-Santana1 <felipe.estudos666@outlook.com>
2022-05-29 23:14:39 -0300

    LAyout padrao de Telas Finalizado

--- LojaDeBrinquedo/src/main/java/loja/Presentation/Cliente.java
+++ LojaDeBrinquedo/src/main/java/loja/Presentation/Cliente.java
@@ -8,7 +8,6 @@ import loja.Dominio.Util.PropertiesValidator;
 import loja.Presentation.Controller.ClienteController;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
